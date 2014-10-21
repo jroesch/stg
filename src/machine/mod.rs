6a930853d87e944e4eaca16baa0b6c28b950cdb9
Jared Roesch <roeschinc@gmail.com>
2014-10-21 02:09:53 -0700

    Remove comments in machine/mod.rs

--- src/machine/mod.rs
+++ src/machine/mod.rs
@@ -11,7 +11,6 @@ pub mod subst;
 pub mod stack;
 pub mod scope;
 
-// macro_rules! rule!
 #[deriving(Show)]
 pub struct Machine {
     static_bindings: Vec<Object>,
@@ -112,104 +111,6 @@ impl Scope for Machine {
     }
 }
 
-// match e {
-//     EAtom(v) => self.top(|k| match k {
-//         _ => None,
-//         Some(Case(alts)) => self.eval(ECase(v, alts))
-//     }),
-//     EAtom(AtomLit(lit)) => fail!(""),
-//     EAtom(AtomVar(ref y)) => match self.stack.pop() {
-//         None => self.invalid_state::<Expr>(),
-//         Some(Update(ref x)) => {
-//             let x_addr = &self.lookup_addr(x);
-//             let y_addr = self.lookup_addr(y);
-//             // y_value should be a value i.e a
-//             *(self.heap.get_mut(x_addr)) = ObjAddr(y_addr);
-//             EAtom(AtomVar(y.clone()))
-//         },
-//         Some(head) => { self.stack.push(head); e.clone() }
-//     },
-//     EFCall(_, ref fun_name, ref args) => {
-//         match self.lookup(fun_name) {
-//             &ObjFun(ref vars, ref expr) => {
-//                 let param_count = vars.len();
-//                 let arg_count = args.len();
-//                 if arg_count == param_count {
-//
-//                 } else if arg_count > param_count {
-//
-//                 } else {
-//
-//                 }; *expr.clone()
-//             },
-//             &ObjPap(ref var, ref atoms) => fail!("pap"),
-//             &ObjThunk(ref expr) => fail!("lol"),
-//             _ => fail!("machine errror")
-//         }
-//     }
-//
-//     EPrimOp(op, atoms) => { fail!("notimpl") },
-//
-//     ELet(v, obj, exp) => {
-//         self.eval_in_scope(vec![(v, obj)], *exp)
-//     }
-//
-//     ECase(EAtom(atom), alts) => {
-//         self.select_match(atom, alts)
-//     }
-//
-//     ECase(expr, alts) => {
-//         self.stack.push(Case(alts));
-//         self.eval(e)
-//     }
-// }
-
-//
-// fn select_match(&mut self, atom: Atom, alts: Vec<Alt>) -> Expr {
-//     // for alt in alts {
-//     //     match atom {
-//     //         AtomLit(lit) => {
-//     //             // self.eval_in_scope(vec![])
-//     //             match alt {
-//     //                 AltName(var, body) =>
-//     //                     self.eval_in_scope(vec![(var, lit)])
-//     //             }
-//     //         }
-//     //
-//     //         AtomVar(var) => fail!("hello")
-//     //     };
-//     //
-//     //     match alt {
-//     //         AltName(name, body) => {
-//     //             self.eval_in_scope(
-//     //                 vec![name, expr],
-//     //                 *expr
-//     //             ),
-//     //         }
-//     //
-//     //         AltCons(cons, bindings, body) => {
-//     //             //match
-//     //         }
-//     //     }
-//     // }
-//     EAtom(atom)
-// }
-
-// /// What to do here?
-// fn top(&mut self, handler: |Kont| -> Option<Expr>) -> Option<Expr> {
-//     self.stack.get(0).map(|k| {
-//         handler(k)
-//     });
-// }
-//
-// fn eval(&mut self, e: Expr) -> Expr {
-//
-// }
-//
-// fn invalid_state<E>(&self) -> E {
-//     fail!("invalid_state!")
-// }
-
 impl Machine {
     pub fn new() -> Machine {
         Machine {
@@ -219,12 +120,6 @@ impl Machine {
             stack: Vec::new(),
             counter: 0
         }
-    }
-
-    fn fresh_addr(&mut self) -> u64 {
-        let fresh_addr = self.counter;
-        self.counter += 1;
-        fresh_addr
     }
 
     pub fn interpret(mut self, Program(bindings): Program) {
@@ -246,6 +141,14 @@ impl Machine {
             },
             _ => fail!("Runtime error")
         };
+    }
+
+    // Utils
+
+    fn fresh_addr(&mut self) -> u64 {
+        let fresh_addr = self.counter;
+        self.counter += 1;
+        fresh_addr
     }
 
     fn current_kont(&self) -> Option<Kont> {
